d8b78711115e7561bb3879f73ce1f1be42977239
alparslanakbas <bybluestht@gmail.com>
2025-01-13 01:04:17 +0300

    Added
    - Added filter

---
+++ MultipleLayered.API/Extensions/Filters/TimeRestrictFilter.cs
@@ -0,0 +1,37 @@
+﻿namespace Multiple_Layered.API.Extensions.Filters
+{
+    public class TimeRestrictFilter : IEndpointFilter
+    {
+        private readonly TimeOnly _startTime;
+        private readonly TimeOnly _endTime;
+        private readonly ILogger<TimeRestrictFilter> _logger;
+
+        public TimeRestrictFilter(ILogger<TimeRestrictFilter> logger,
+            string startTime = "09:00", string endTime = "17:00")
+        {
+            _logger = logger;
+            _startTime = TimeOnly.Parse(startTime);
+            _endTime = TimeOnly.Parse(endTime);
+        }
+
+        public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+        {
+            var currentTime = TimeOnly.FromDateTime(DateTime.Now);
+
+            if (currentTime < _startTime || currentTime > _endTime)
+            {
+                _logger.LogWarning("Mesai saatleri dışında erişim denemesi: {Time}", currentTime);
+
+                return Results.Json(new
+                {
+                    Error = "Bu API'ye sadece mesai saatleri içinde erişilebilir, eğer bu ayarı kapatmak istiyorsanız endpointten filterı kaldırın.",
+                    WorkingHours = $"{_startTime} - {_endTime}",
+                    CurrentTime = currentTime.ToString()
+                }, statusCode: StatusCodes.Status403Forbidden);
+            }
+
+            _logger.LogInformation("API erişimi başarılı: {Time}", currentTime);
+            return await next(context);
+        }
+    }
+}
